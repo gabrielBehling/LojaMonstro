a44fc09550320a02e525149308a511afd867921d
Tyxiel <128870773+Tyxiel@users.noreply.github.com>
2024-11-11 13:58:52 -0300

    Fix active/open duplicated, remove duplicated and unused script, and unnecessary comments.

--- js/default-script.js
+++ js/default-script.js
@@ -2,25 +2,20 @@
 function toggleSubNav() {
   const subNavigation = document.querySelector(".sub-navigation");
   const hamburger = document.querySelector(".hamburger-menu");
-  const icon = hamburger.querySelector("i"); // Select the icon inside the hamburger menu
+  const icon = hamburger.querySelector("i");
 
   subNavigation.classList.toggle("open");
   hamburger.classList.toggle("open");
 
   // Change the icon based on the state of the sub-navigation
   if (subNavigation.classList.contains("open")) {
-    icon.classList.remove("bx-menu"); // Remove the hamburger icon
-    icon.classList.add("bx-x"); // Add the close icon
+    icon.classList.remove("bx-menu");
+    icon.classList.add("bx-x");
   } else {
-    icon.classList.remove("bx-x"); // Remove the close icon
-    icon.classList.add("bx-menu"); // Add the hamburger icon back
+    icon.classList.remove("bx-x");
+    icon.classList.add("bx-menu");
   }
 }
-
-// Adiciona o evento de clique no ícone do hambúrguer
-document
-  .querySelector(".hamburger-menu")
-  .addEventListener("click", toggleSubNav);
 
 // Adiciona o evento de clique no ícone do hambúrguer
 document
@@ -34,28 +29,12 @@ document.querySelectorAll(".sub-navigation a").forEach((anchor) => {
       e.preventDefault();
       const targetId = this.getAttribute("href");
       const targetElement = document.querySelector(targetId);
-      const headerOffset = 80;
+      const headerOffset = 75;
       const elementPosition =
         targetElement.getBoundingClientRect().top + window.scrollY;
       const offsetPosition = elementPosition - headerOffset;
       window.scrollTo({ top: offsetPosition, behavior: "smooth" });
       toggleSubNav();
     });
-  }
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  const hamburgerMenu = document.getElementById("hamburger-menu");
-  const subNavigation = document.querySelector(".sub-navigation");
-
-  // Verificar se os elementos estão presentes no DOM
-  if (hamburgerMenu && subNavigation) {
-    hamburgerMenu.addEventListener("click", () => {
-      // Alterna a classe 'active' no menu
-      subNavigation.classList.toggle("active");
-      hamburgerMenu.classList.toggle("active");
-    });
-  } else {
-    console.error("Erro: Elemento do menu ou navegação não encontrado");
   }
 });
