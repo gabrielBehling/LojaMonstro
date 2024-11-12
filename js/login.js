021d4bc5c71b08f308dfabacca57e104c3cc6d52
Tyxiel <128870773+Tyxiel@users.noreply.github.com>
2024-11-12 16:39:19 -0300

    Almost finished login page

--- js/login.js
+++ js/login.js
@@ -1,10 +1,23 @@
 const loginForm = document.querySelector(".login-form");
-      const signupForm = document.querySelector(".signup-form");
-      const toggleForms = document.querySelectorAll(".toggle-form");
+const signupForm = document.querySelector(".signup-form");
+const toggleForms = document.querySelectorAll(".toggle-form");
 
-      toggleForms.forEach((toggle) => {
-        toggle.addEventListener("click", () => {
-          loginForm.classList.toggle("active");
-          signupForm.classList.toggle("active");
-        });
-      });
+toggleForms.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    if (loginForm.classList.contains("active")) {
+      // Fechar o formulário de login
+      loginForm.classList.add("closing");
+      setTimeout(() => {
+        loginForm.classList.remove("active", "closing");
+        signupForm.classList.add("active");
+      }, 500); // Tempo da animação
+    } else {
+      // Fechar o formulário de criação de conta
+      signupForm.classList.add("closing");
+      setTimeout(() => {
+        signupForm.classList.remove("active", "closing");
+        loginForm.classList.add("active");
+      }, 500); // Tempo da animação
+    }
+  });
+});
